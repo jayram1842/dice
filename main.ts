3e13cb603213874ca30742853163fb978c0b5b8a
jayram1842 <112582510+jayram1842@users.noreply.github.com>
2022-08-31 12:14:45 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let rolls = 0
+input.onGesture(Gesture.Shake, function () {
+    rolls = randint(1, 6)
+    if (rolls == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (rolls == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (rolls == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (rolls == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (rolls == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
